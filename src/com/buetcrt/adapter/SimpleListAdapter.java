bb0cb843b2e4d8b05161f57e1a5246d6560c8be7
tanzeerH <tanzeer_s@yahoo.com>
2015-05-07 21:17:54 +0600

    simple list adapter update

---
+++ src/com/buetcrt/adapter/SimpleListAdapter.java
@@ -0,0 +1,51 @@
+package com.buetcrt.adapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.buet_crt.csefest.R;
+
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SimpleListAdapter extends ArrayAdapter<String> {
+
+	private Context mContext;
+	public SimpleListAdapter(Context context, int resource, ArrayList<String> objects) {
+		super(context, resource, objects);
+		this.mContext=context;
+	}
+	private class ViewHolder{
+		TextView tvName;
+	}
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder=null;
+            LayoutInflater mInflater = (LayoutInflater) mContext.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+    		if (convertView == null) {
+    			
+    				 convertView = mInflater.inflate(R.layout.row_simple_list, null);
+    			
+    			holder = new ViewHolder();
+    			holder.tvName = (TextView) convertView.findViewById(R.id.tvop);
+  
+    			convertView.setTag(holder);
+    		} else {
+    			holder = (ViewHolder) convertView.getTag();
+    		}
+    	holder.tvName.setText(getItem(position));
+  
+            		
+		
+		return convertView;
+	}
+
+}
